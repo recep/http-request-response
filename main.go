63802d6b72e911ab9d806e5b62e920cf448c2e30
Recep Alaca <recepalacaa@gmail.com>
2020-05-01 13:40:57 +0300

    terminal design updated

--- main.go
+++ main.go
@@ -19,8 +19,8 @@ func main() {
 	w := string(file)
 	websites := []string(strings.Fields(w))
 
-	fmt.Printf("%-10s %-10s %-10s\n", "Website", "Status", "Status Text")
-	fmt.Println(strings.Repeat("-", 30))
+	fmt.Printf("%-30s %-10s %-10s\n", "Website", "Status", "Status Text")
+	fmt.Println(strings.Repeat("-", 55))
 
 	for _, website := range websites {
 
@@ -29,7 +29,7 @@ func main() {
 			fmt.Println("Error: ", err)
 		}
 
-		fmt.Printf("%s %d %s\n", website, resp.StatusCode, http.StatusText(resp.StatusCode))
+		fmt.Printf("%-30s %-10d %-10s\n", website, resp.StatusCode, http.StatusText(resp.StatusCode))
 	}
 
 }
